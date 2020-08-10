517482c804ff596ee898ffb6e7560579d4fca6ea
Lucia amanda Boza barajas <lboza-ba@c2r9s3.42madrid.com>
2020-08-10 14:56:41 +0200

    ElSencillo

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,11 @@
 /*   By: lboza-ba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/27 11:40:24 by lboza-ba          #+#    #+#             */
-/*   Updated: 2020/08/09 22:43:09 by lboza-ba         ###   ########.fr       */
+/*   Updated: 2020/08/10 14:36:08 by lboza-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "stdio.h"
-
-size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	int cont;
-
-	cont = 0;
-	if (!dst || !src)
-		return(0);
-	while (src[cont] != '\0')
-		cont++;
-	while (dstsize > 1 && *src != '\0')
-	{
-		*dst = *src;
-		dst++;
-		src++;
-		dstsize--;
-	}
-	if (dstsize != 0)
-		*dst = '\0';
-	return (cont);
-}
 
 char				*ft_strjoin(char const *s1, char const *s2)
 {
@@ -71,27 +49,23 @@ int					get_line(char *buffer, char **line)
 
 	i = 0;
 	buf = buffer;
-	if (buffer == NULL)
+	if (buf == NULL)
 		return (-1);
-	if (!(mo = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char))))
+	if (!(mo = (char*)malloc(BUFFER_SIZE + 1 * sizeof(char))))
 		return (-1);
 	while (*buf != '\n' && *buf != '\0')
 		*(mo+i++) = *buf++;
 	*(mo+i) = '\0';
-	printf("El contenido de line antes de strjoin es: %s\n", *line);
 	*line = ft_strjoin(*line, mo);
-	printf("El contenido de line despues de strjoin es: %s\n", *line);
 	free(mo);
 	if (*buf == '\0')
 	{
-		free((void*)buffer);
+		//free((void*)now_red->buf);
 		return (0);
 	}
 	else
 	{
-		printf("El buffer antes del strlcpy es: %s\n", buffer);
 		ft_strlcpy(buffer, ++buf, BUFFER_SIZE);
-		printf("El buffer despues del strlcpy es: %s\n", buffer);
 		return (1);
 	}
 }
@@ -102,16 +76,13 @@ int					get_buffer_line(int fd, char *buffer, char **line)
 	int		readed;
 	int		i;
 
-	i = 0;
 	end_line = 0;
 	while (end_line == 0)
 	{
-		if (!(buffer = (char*)malloc((BUFFER_SIZE + 1) * sizeof(char))))
-			return (-1);
-		while (i <=BUFFER_SIZE)
+		i = 0;
+		while (i <= BUFFER_SIZE)
 			buffer[i++] = '\0';
 		readed = read(fd, buffer, BUFFER_SIZE);
-		printf("El contenido de buffer después de leer es: %s\n", buffer);
 		if (readed < 0)
 		{
 			*line = NULL;
@@ -127,11 +98,12 @@ int					get_buffer_line(int fd, char *buffer, char **line)
 
 int					get_next_line(int fd, char **line)
 {
-	static char	*buffer;
-	int			returning;
+	static char	buffer[BUFFER_SIZE + 1	];
+	int				returning;
 
 	if (line == NULL || BUFFER_SIZE < 1)
 		return (-1);
+
 	if (!(*line = (char*)malloc(1 * sizeof(char))))
 		return (-1);
 	**line = '\0';
@@ -139,6 +111,8 @@ int					get_next_line(int fd, char **line)
 	if (get_line(buffer, line) != 1)
 		returning = get_buffer_line(fd, buffer, line);
 	if (returning < 1)
-		free(buffer);
+	{
+		//buffer = NULL;
+	}
 	return (returning);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lboza-ba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/27 11:50:55 by lboza-ba          #+#    #+#             */
-/*   Updated: 2020/08/09 22:43:40 by lboza-ba         ###   ########.fr       */
+/*   Updated: 2020/08/09 19:43:40 by lboza-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,12 @@
 # include <unistd.h>
 
 int					get_next_line(int fd, char **line);
+size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
+typedef	struct		s_buff_file {
+	int					fd;
+	char				*buf;
+	struct s_buff_file	*next;
+}				t_file;
+struct s_buff_file	*ft_freelist(t_file *reading, t_file *now_reading);
 
 #endif
